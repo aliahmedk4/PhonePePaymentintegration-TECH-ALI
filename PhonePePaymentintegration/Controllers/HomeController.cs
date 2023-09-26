290944c6d715a4057e4daaba52e0acdb0ca3255b
ali <ali.tech1108@gmail.com>
2023-09-26 14:31:43 +0530

    Add project files.

---
+++ PhonePePaymentintegration/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhonePePaymentintegration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: /Home/GeneratePaymentLink
+        [HttpPost]
+        public async Task<JsonResult> GeneratePaymentLink(VerifyRequestModel phonePePayment)
+        {
+            try
+            {
+                // ON LIVE URL YOU MAY GET CORS ISSUE, ADD Below LINE TO RESOLVE
+                //ServicePointManager.Expect100Continue = true;
+                //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                var PhonePeGatewayURL = "https://api-preprod.phonepe.com/apis/pg-sandbox";
+
+                var httpClient = new HttpClient();
+                var uri = new Uri($"{PhonePeGatewayURL}/pg/v1/pay");
+
+                // Add headers
+                httpClient.DefaultRequestHeaders.Add("accept", "application/json");
+                httpClient.DefaultRequestHeaders.Add("X-VERIFY", phonePePayment.X_VERIFY);
+
+                // Create JSON request body
+                var jsonBody = $"{{\"request\":\"{phonePePayment.base64}\"}}";
+                var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+                // Send POST request
+                var response = await httpClient.PostAsync(uri, content);
+                response.EnsureSuccessStatusCode();
+
+                // Read and deserialize the response content
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                // Return a response
+                return Json(new { Success = true, Message = "Verification successful",phonepeResponse= responseContent });
+            }
+            catch (Exception ex)
+            {
+                // Handle errors and return an error response
+                return Json(new { Success = false, Message = "Verification failed", Error = ex.Message });
+            }
+        }
+
+        // POST: /Home/CheckPaymentStatus
+        [HttpPost]
+        public async Task<JsonResult> CheckPaymentStatus(VerifyRequestModel phonePePayment)
+        {
+            try
+            {
+                // ON LIVE URL YOU MAY GET CORS ISSUE, ADD Below LINE TO RESOLVE
+                //ServicePointManager.Expect100Continue = true;
+                //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                var PhonePeGatewayURL = "https://api-preprod.phonepe.com/apis/pg-sandbox";
+
+                var httpClient = new HttpClient();
+                var uri = new Uri($"{PhonePeGatewayURL}/pg/v1/status/{phonePePayment.MERCHANTID}/{phonePePayment.TransactionId}");
+
+                // Add headers
+                httpClient.DefaultRequestHeaders.Add("accept", "application/json");
+                httpClient.DefaultRequestHeaders.Add("X-VERIFY", phonePePayment.X_VERIFY);
+                httpClient.DefaultRequestHeaders.Add("X-MERCHANT-ID", phonePePayment.MERCHANTID);
+
+                // Create JSON request body
+
+                // Send POST request
+                var response = await httpClient.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+
+                // Read and deserialize the response content
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                // Return a response
+                return Json(new { Success = true, Message = "Verification successful", phonepeResponse = responseContent });
+            }
+            catch (Exception ex)
+            {
+                // Handle errors and return an error response
+                return Json(new { Success = false, Message = "Verification failed", Error = ex.Message });
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    public class VerifyRequestModel
+    {
+        public string X_VERIFY { get; set; }
+        public string base64 { get; set; }
+        public string TransactionId { get; set; }
+        public string MERCHANTID { get; set; }
+        // Add other properties from the request if needed
+    }
+}
---
+++ PhonePePaymentintegration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhonePePaymentintegration.MvcApplication" Language="C#" %>
